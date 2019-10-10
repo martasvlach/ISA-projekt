d39a1e05c11910c03c8f7a3b6d6ff3d212853dfa
Martin Vlach Ubuntu <xvlach18@stud.fit.vutbr.cz>
2019-10-10 12:17:14 +0200

    50 % funkcionality cca ok, nakonec místo procesů pracováno s vlákny

---
+++ WorkingISA/Client/cmake-build-debug/test.php
@@ -0,0 +1,7 @@
+<?php
+
+
+for ($i = 0; $i < 69000; $i++)
+{
+    exec("./Client -p 12345 -H localhost boards");
+}
---
+++ WorkingISA/isaserver/cmake-build-debug/CMakeFiles/isaserver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/isaserver.dir/isaserver.cpp.o"
+  "isaserver"
+  "isaserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/isaserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
